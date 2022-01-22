912f27e77145700ea78527a89f7a02dcf7c97cac
marjoryebrito <marjoryebrito@protonmail.com>
2022-01-22 16:25:27 -0300

    Redirecionamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -44,4 +44,13 @@ Route::prefix('/app')-> group(function(){
 });
 
 
+//Redirecionamemento de rotas
+Route::get('/rota1', function(){
+    echo 'Rota 1';
+})-> name('site.rota1');
 
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})-> name('site.rota2');
+
+//Route::redirect('/rota2', '/rota1');
