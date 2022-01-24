8dcf5ac1ee9405a6fd79d55c80ad3d1c59c5c6b7
marjoryebrito <marjoryebrito@protonmail.com>
2022-01-24 09:52:36 -0300

    Blade - Incluindo comentários e blocos PHP puros

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,13 @@
+<h3>Fornecedor</h3>
+
+{{-- Fica o comentário que será descartado pelo interpretador do Blade --}}
+
+@php
+
+//Para comentários de 1 linha
+
+/*
+Para comentários com mais de 1 linha
+*/
+echo "Texto de teste";
+@endphp
