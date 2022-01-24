333b36bf1b382211b24bbdbdcd47c4cdeb239ba1
marjoryebrito <marjoryebrito@protonmail.com>
2022-01-24 12:08:33 -0300

    Blade - '@empty'

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -16,11 +16,14 @@
 
 @isset($fornecedores) 
 
-Fornecedor: {{$fornecedores[1]['nome']}} <br/>
+Fornecedor: {{$fornecedores[0]['nome']}} <br/>
 
-Status: {{$fornecedores[1]['status']}}<br/>
+Status: {{$fornecedores[0]['status']}}<br/>
 
-@isset($fornecedores[1]['cnpj'])
-CNPJ: {{$fornecedores[1]['cnpj']}}<br/>
+@isset($fornecedores[0]['cnpj'])
+CNPJ: {{$fornecedores[0]['cnpj']}}
+    @empty($fornecedores[0]['cnpj'])
+        - Vazio
+    @endempty    
 @endisset
 @endisset
