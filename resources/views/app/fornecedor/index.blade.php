77cab7dec348f6017c3dd9564fe4ad5558da79a6
marjoryebrito <marjoryebrito@protonmail.com>
2022-01-24 10:38:48 -0300

    Blade - @if/@else

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -11,3 +11,14 @@ Para comentários com mais de 1 linha
 */
 echo "Texto de teste";
 @endphp
+
+{{-- @dd($fornecedores) // exibindo array através do Blade--}}
+
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>    
+@endif    
