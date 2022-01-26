2ec55a670cc2909d9de5e5ad3cd65e81acaa9933
marjoryebrito <marjoryebrito@protonmail.com>
2022-01-25 23:30:05 -0300

    Enviando o formulário de contato via POST

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,9 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos')-> name('site.sobrenos');
 
 Route::get('/contato', 'ContatoController@contato')-> name('site.contato');
 
+Route::post('/contato', 'ContatoController@contato')-> name('site.contato');
+
+
 Route::get('/login', function(){return 'Login';})-> name('site.login');
 
 
