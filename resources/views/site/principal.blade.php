5fe129921ccef14ed8c44950399ce1412268c430
marjoryebrito <marjoryebrito@protonmail.com>
2022-01-29 09:09:21 -0300

    Ajustando o formulário de contato na rota principal

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -31,7 +31,7 @@
         <h1>Contato</h1>
         <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
        
-        @component('site.layout._components.form_contato', ['classe' => 'borda-branca'])
+        @component('site.layout._components.form_contato', ['classe' => 'borda-branca', 'motivos_contato'=>$motivos_contato])
        @endcomponent
     </div>
 </div>
