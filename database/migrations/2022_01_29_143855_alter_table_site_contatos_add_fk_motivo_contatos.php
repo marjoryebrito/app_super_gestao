b2a9134b15cb3b35f24dad01d6f1510e2dcf6a99
marjoryebrito <marjoryebrito@protonmail.com>
2022-01-29 12:13:51 -0300

    Refactoring do projeto Super Gestão parte 2

---
+++ database/migrations/2022_01_29_143855_alter_table_site_contatos_add_fk_motivo_contatos.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableSiteContatosAddFkMotivoContatos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Adicionando a coluna motivo_contatos_id
+        Schema::table('site_contatos', function(Blueprint $table){
+            $table->unsignedBigInteger('motivo_contatos_id');
+        });
+
+        //Atribuindo motivo_contato para a nova coluna motivo_contatos_id
+        DB::statement('update site_contatos set motivo_contatos_id = motivo_contato');
+
+        //Constraint (fk) e exclusão da coluna motivo_contato
+        Schema::table('site_contatos', function(Blueprint $table){
+            $table->foreign('motivo_contatos_id')->references('id')->on('motivo_contatos');
+            $table->dropColumn('motivo_contato');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //Desfazendo exclusão da coluna motivo_contato e constraint (fk)
+        Schema::table('site_contatos', function(Blueprint $table){
+            $table->integer('motivo_contato');
+            $table->dropForeign('site_contatos_motivo_contatos_id_foreign');
+        });
+
+
+        //Desfazendo a atribuição motivo_contato para a nova coluna motivo_contatos_id
+      
+        DB::statement('update site_contatos set motivo_contato = motivo_contatos_id');
+
+        //Excluindo a coluna motivo_contatos_id
+
+        Schema::table('site_contatos', function(Blueprint $table){
+            $table->dropColumn('motivo_contatos_id');
+        });
+    }
+}
