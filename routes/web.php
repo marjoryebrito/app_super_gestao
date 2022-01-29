f9f4b1f7a1e15a49fc8040fb3bee59ef7187d048
marjoryebrito <marjoryebrito@protonmail.com>
2022-01-29 15:29:21 -0300

    Criando meu primeiro middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Middleware\LogAcessoMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,7 +24,9 @@ Route::get('/', [\App\Http\Controller\PrincipalController::class, 'principal']);
 */
 
 
-Route::get('/', 'PrincipalController@principal')-> name('site.index');
+Route::middleware(LogAcessoMiddleware::class)
+-> get('/', 'PrincipalController@principal')
+-> name('site.index');
 
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')-> name('site.sobrenos');
 
