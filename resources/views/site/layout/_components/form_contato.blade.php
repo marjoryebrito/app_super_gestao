39488d8b1811c988043d1eb5f9c9e85080cd9788
marjoryebrito <marjoryebrito@protonmail.com>
2022-01-29 14:20:18 -0300

    Customizando a apresentação dos erros de validação parte 2

--- resources/views/site/layout/_components/form_contato.blade.php
+++ resources/views/site/layout/_components/form_contato.blade.php
@@ -2,11 +2,19 @@
 <form action="{{ route('site.contato') }}" method="post">
 @csrf
     <input name="nome" value="{{ old('nome')}}" type="text" placeholder="Nome" class="{{$classe}}">
+    @if ($errors->has('nome'))
+        {{$errors->first('nome')}}
+    @endif
     <br>
+
     <input name="telefone" value="{{ old('telefone')}}" type="text" placeholder="Telefone" class="{{$classe}}">
+    {{$errors->has('telefone') ? $errors->first('telefone') : ''}}
     <br>
+
     <input name="email" value="{{ old('email')}}" type="text" placeholder="E-mail" class="{{$classe}}">
+    {{$errors->has('email') ? $errors->first('email') : ''}}
     <br>
+
     <select name="motivo_contatos_id" class="{{$classe}}">
         <option value="">Qual o motivo do contato?</option>
 
@@ -15,9 +23,13 @@
         @endforeach
         
     </select>
+    {{$errors->has('motivo_contato_id') ? $errors->first('motivo_contato_id') : ''}}
     <br>
+
     <textarea name="mensagem" class="{{$classe}}">@if(old('mensagem') != ''){{ old('mensagem') }}@else Preencha aqui a sua mensagem @endif</textarea>
+    {{$errors->has('mensagem') ? $errors->first('mensagem') : ''}}
     <br>
+
     <button type="submit" class="{{$classe}}">ENVIAR</button>
 </form>
 
