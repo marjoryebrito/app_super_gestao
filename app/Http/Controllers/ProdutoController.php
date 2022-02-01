2e4c891c42acd5e5a3814045bda2eb5b16db0872
marjoryebrito <marjoryebrito@protonmail.com>
2022-02-01 19:31:57 -0300

    Implementando o cadastro de produtos parte 5 (show).

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -67,7 +67,7 @@ class ProdutoController extends Controller
      */
     public function show(Produto $produto)
     {
-        //
+        return view('app.produto.show', ['produto'=>$produto]);
     }
 
     /**
@@ -78,7 +78,7 @@ class ProdutoController extends Controller
      */
     public function edit(Produto $produto)
     {
-        //
+        
     }
 
     /**
