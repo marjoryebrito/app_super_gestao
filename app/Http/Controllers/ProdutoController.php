36a95503904f708e33431029bd6b0c9d3d05fff3
marjoryebrito <marjoryebrito@protonmail.com>
2022-02-07 15:06:18 -0300

    Eloquent ORM 1 para 1 - Exibindo os detalhes do produto.

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Produto;
 use \App\Unidade;
+use \App\ProdutoDetalhe;
 use Illuminate\Http\Request;
 
 
@@ -17,6 +18,20 @@ class ProdutoController extends Controller
     public function index(Request $request)
     {
         $produtos = Produto::paginate(10);
+
+        foreach($produtos as $key => $produto){
+            $produtoDetalhe = ProdutoDetalhe::where('produto_id', $produto->id)->first();
+
+            if(isset($produtoDetalhe)){
+                $produtos[$key]['comprimento'] = $produtoDetalhe->comprimento;
+                $produtos[$key]['largura'] = $produtoDetalhe->largura;
+                $produtos[$key]['altura'] = $produtoDetalhe->altura;
+               
+            }
+
+        }
+
+       
 
         return view('app.produto.index', ['produtos'=> $produtos , 'request'=>$request->all()]);
     }
