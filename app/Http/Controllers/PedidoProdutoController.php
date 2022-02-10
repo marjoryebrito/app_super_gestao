51d88553c04a0e042f276cc8c3ce0da5b1f75400
marjoryebrito <marjoryebrito@protonmail.com>
2022-02-09 22:14:19 -0300

    Eloquent ORM N para N - Implementando o relacionamento belongsToMany.

--- app/Http/Controllers/PedidoProdutoController.php
+++ app/Http/Controllers/PedidoProdutoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use \App\Pedido;
 use \App\Produto;
+use \App\PedidoProduto;
 
 class PedidoProdutoController extends Controller
 {
@@ -26,6 +27,7 @@ class PedidoProdutoController extends Controller
     public function create(Pedido $pedido)
     {
         $produtos = Produto::all();
+       // $pedido->produtos; //Eager loading
         return view('app.pedido_produto.create', ['pedido'=>$pedido, 'produtos'=>$produtos]);
     }
 
@@ -37,7 +39,25 @@ class PedidoProdutoController extends Controller
      */
     public function store(Request $request, Pedido $pedido)
     {
-        
+
+       
+
+        $regras = [
+            'produto_id' => 'exists:produtos,id'
+        ];
+
+        $feedback = [
+            'produto_id.exists' => 'O produto informado não existe.'
+        ];
+
+        $request->validate($regras, $feedback);
+
+        $pedidoProduto = new PedidoProduto();
+        $pedidoProduto->pedido_id = $pedido->id;
+        $pedidoProduto->produto_id = $request->get('produto_id');
+        $pedidoProduto->save();
+
+        return redirect()->route('pedido-produto.create', ['pedido'=>$pedido->id]);
     }
 
     /**
--- app/Pedido.php
+++ app/Pedido.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
-    //
+    public function produtos(){
+       // return $this->belongsToMany('\App\Produto', 'pedido_produtos');
+
+       return $this->belongsToMany('\App\Item', 'pedido_produtos', 'pedido_id', 'produto_id');
+    }
 }
